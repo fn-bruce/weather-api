ca34a1d5f93c439af81e924b6a8d4c1b5dc55998
Brucelee Nguyen <bruceleenguyen95@gmail.com>
2020-12-23 01:25:12 -0700

    implemented an elapsed time decorator to main. implemented queues

--- weather_forecast_average.py
+++ weather_forecast_average.py
@@ -1,9 +1,19 @@
+import queue
 import threading
 import requests
 import json
+import time
 
+def elapsed_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        func(*args, **kwargs)
+        print(f"Elapsed Time: {time.time() - start}")
 
-def get_avg_max_temp(url):
+    return wrapper
+
+def get_avg_max_temp(q):
+    url = q.get()
     req = requests.get(url)
     weather_data = json.loads(req.text)
     city = weather_data["title"]
@@ -12,17 +22,28 @@ def get_avg_max_temp(url):
     ]
     avg_max_temp = round(sum(max_temps) / len(max_temps), 2)
     print(f"{city} Average Max Temp: {avg_max_temp}")
+    q.task_done()
 
 
+@elapsed_time
 def main():
     urls = [
         "https://www.metaweather.com/api/location/2487610/",
         "https://www.metaweather.com/api/location/2442047/",
         "https://www.metaweather.com/api/location/2366355/",
     ]
-    for url in urls:
-        t = threading.Thread(target=get_avg_max_temp, args=(url,))
+    q = queue.Queue()
+    work_queue = queue.Queue(3)
+    threads = []
+    for i in range(len(urls)):
+        t = threading.Thread(target=get_avg_max_temp, args=(q,))
+        t.daemon = True
         t.start()
+
+    for url in urls:
+        q.put(url)
+
+    q.join()
 
 
 if __name__ == "__main__":
